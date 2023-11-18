bb0cebc5e942c2fae856a1e5d3dd9a5165c16f49
dgotal <93431548+dgotal@users.noreply.github.com>
2023-11-18 10:10:53 +0100

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,47 @@
 import streamlit as st
 from PIL import Image
-import requests
-from io import BytesIO
 
 def classify_emotion(image):
     return "Happy"
 
 def main():
-    st.title("Emotion Classification App")
+    st.set_page_config(
+        page_title="Emotion Classification App",
+        page_icon=":smiley:",
+        layout="wide"
+    )
 
-    uploaded_files = st.file_uploader("Choose one or more images...", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+    st.sidebar.title("Navigation")
+    page_options = ["Home", "About"]
+    page = st.sidebar.radio("Go to", page_options)
 
-    if uploaded_files:
-        for uploaded_file in uploaded_files:
-            image = Image.open(uploaded_file)
-            
-            st.image(image, caption="Uploaded Image", use_column_width=True)
+    if page == "Home":
+        st.title("Emotion Classification App")
 
-            emotion = classify_emotion(image)
+        uploaded_files = st.file_uploader("Choose one or more images...", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 
-            st.write(f"Emotion: {emotion}")
+        if uploaded_files:
+            for uploaded_file in uploaded_files:
+                st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
 
-    if uploaded_files and st.button("Generate Statistics"):
-        emotions = [classify_emotion(Image.open(file)) for file in uploaded_files]
-        emotion_counts = {emotion: emotions.count(emotion) for emotion in set(emotions)}
+                emotion = classify_emotion(Image.open(uploaded_file))
 
-        st.write("### Emotion Statistics:")
-        for emotion, count in emotion_counts.items():
-            st.write(f"{emotion}: {count} image(s)")
+                st.write(f"**Emotion:** {emotion}")
+
+        if uploaded_files and st.button("Generate Statistics"):
+            emotions = [classify_emotion(Image.open(file)) for file in uploaded_files]
+            emotion_counts = {emotion: emotions.count(emotion) for emotion in set(emotions)}
+
+            st.write("### Emotion Statistics:")
+            for emotion, count in emotion_counts.items():
+                st.write(f"{emotion}: {count} image(s)")
+
+    elif page == "About":
+        st.title("About This App")
+        st.write("This is a simple Streamlit app for classifying emotions in images.")
+
+    st.markdown("---")
+    st.write("© 2023 Emotion Classification App. All rights reserved.")
 
 if __name__ == "__main__":
     main()
