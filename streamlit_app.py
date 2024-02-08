71a82eab0a0c167ffdb559cd060f61a35abad258
dgotal <93431548+dgotal@users.noreply.github.com>
2024-02-08 17:09:24 +0100

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,10 @@ def extract_features(image, model):
     return features
 
 
-cnn_model = load_model('EmoClassify/CNN_model.h5')
-cnn2_model = load_model('EmoClassify/CNN2_model.h5')
-dtree_model = joblib.load('EmoClassify/decisionTreeModel.joblib')
-kmeans_model = joblib.load('EmoClassify/emotion_kmeans_model.pkl')
+cnn_model = load_model('CNN_model.h5')
+cnn2_model = load_model('CNN2_model.h5')
+dtree_model = joblib.load('decisionTreeModel.joblib')
+kmeans_model = joblib.load('emotion_kmeans_model.pkl')
 
 models = {
     'CNN': cnn_model,
